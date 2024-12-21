a4371d9704b8da7bb6a4136d7b5675912361716d
29-29 <ackx29sodo@gmail.com>
2024-12-21 12:25:01 +0800

    added input fields

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,43 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const inputTodo = ref<string>()
+</script>
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page class="q-pa-md column">
+    <div class="row q-gutter-sm">
+      <q-input outlined dense v-model="inputTodo" label="Todo" />
+      <q-btn round flat dense icon="add" size="sm" />
+    </div>
   </q-page>
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue';
-import type { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
-
-const meta = ref<Meta>({
-  totalCount: 1200
-});
-</script>
